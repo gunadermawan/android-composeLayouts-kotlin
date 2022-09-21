252b1876ea46374051905a6822783272c9852719
itselaras <gunadermawan62@gmail.com>
2022-09-21 23:25:40 +0700

    first init

---
+++ app/src/main/java/com/gunder/composelayouts/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.gunder.composelayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(msg = Message("Guna", "hello theres! i'm using compose to build android apps!"))
+}
+
