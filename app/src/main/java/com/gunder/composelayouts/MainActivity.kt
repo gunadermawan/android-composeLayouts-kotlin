eb2e0468f563028119d3c086c7e29d4a4478f744
itselaras <gunadermawan62@gmail.com>
2022-09-21 23:31:49 +0700

    add an image element

--- app/src/main/java/com/gunder/composelayouts/MainActivity.kt
+++ app/src/main/java/com/gunder/composelayouts/MainActivity.kt
@@ -3,9 +3,12 @@ package com.gunder.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +24,12 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Image(painter = painterResource(R.drawable.ic_account), contentDescription = "account logo")
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
