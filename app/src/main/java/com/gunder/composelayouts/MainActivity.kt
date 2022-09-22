3bee392820c485205e3b6da9802dee1b7c283d47
itselaras <gunadermawan62@gmail.com>
2022-09-22 09:17:15 +0700

    add a shape and theme configuration

--- app/src/main/java/com/gunder/composelayouts/MainActivity.kt
+++ app/src/main/java/com/gunder/composelayouts/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.gunder.composelayouts
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -36,15 +39,30 @@ fun MessageCard(msg: Message) {
         Image(painter = painterResource(R.drawable.ic_account), contentDescription = "account logo",
             modifier = Modifier
                 .size(40.dp)
-                .clip(CircleShape))
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2)
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp) {
+                Text(text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp))
+            }
         }
     }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 
 @Preview
 @Composable
